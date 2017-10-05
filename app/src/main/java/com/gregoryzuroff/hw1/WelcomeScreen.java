fe861ff8689b79af56462b37dab8d2b465b0a9c7
Gregory Zuroff <gregoryzuroff@0587374187.wireless.umich.net>
2017-10-05 14:46:14 -0400

    first homework done

--- app/src/main/java/com/gregoryzuroff/hw1/WelcomeScreen.java
+++ app/src/main/java/com/gregoryzuroff/hw1/WelcomeScreen.java
@@ -2,12 +2,39 @@ package com.gregoryzuroff.hw1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class WelcomeScreen extends Activity {
+public class WelcomeScreen extends Activity implements Button.OnClickListener{
+
+    private Button buttonLogIn;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        buttonLogIn = (Button) findViewById(R.id.buttonLogIn);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonLogIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == buttonLogIn){
+            if(editTextUsername.getText().toString().equals("ross") && editTextPassword.getText().toString().equals("to426")){
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Access Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
