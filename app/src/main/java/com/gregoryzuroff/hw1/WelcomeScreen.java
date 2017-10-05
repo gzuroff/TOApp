73c71808e9f2cb206baf6f2aaf4bf92c6d03d543
Gregory Zuroff <gregoryzuroff@0587374187.wireless.umich.net>
2017-10-05 14:08:40 -0400

    first commit

---
+++ app/src/main/java/com/gregoryzuroff/hw1/WelcomeScreen.java
@@ -0,0 +1,13 @@
+package com.gregoryzuroff.hw1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
